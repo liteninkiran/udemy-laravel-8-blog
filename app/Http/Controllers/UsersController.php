82f0c380c385806d13909cd37f3496914863d4f6
Kiran Anand <liteninkiran@hotmail.com>
2021-09-01 23:25:46 +0100

    Add Users to Database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function userForm() {
         return view('create_user');
+    }
+
+    public function store(Request $request) {
+        User::create($request->all());
+        return redirect('/');
     }
 }
--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -1,11 +1,15 @@
 <x-layout>
+
     <x-slot name="title">
         Create User Form
     </x-slot>
 
     <form action="{{ route('user.store') }}" method="POST">
+        @csrf
         <input type="text" name="name" placeholder="Enter your name">
-        <input type="text" name="email" placeholder="Enter your email">
+        <input type="email" name="email" placeholder="Enter your email">
+        <input type="password" name="password" placeholder="Enter password">
         <input type="submit">
     </form>
+
 </x-layout>
